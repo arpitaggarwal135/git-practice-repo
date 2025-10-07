dbc8e7a15f0be6d28af8f76c019357a2b4c2b39c
Arpit <arpitaggarwal135@gmailcom>
2025-10-07 13:41:08 +0530

    dflf

--- src/main/java/in/ashokit/constants/KafkaConsumerConstants.java
+++ src/main/java/in/ashokit/constants/KafkaConsumerConstants.java
@@ -4,5 +4,4 @@ public class KafkaConsumerConstants {
 	public static final String TOPIC = "tracking";
 	public static final String GROUP_ID = "group_tracking";
 	public static final String HOST = "localhost:9092";
-	public static final String MODEL = "Hazel";
 }
